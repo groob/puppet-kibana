0e2f0a7d1a7dbf191f5f50c578e3d3d3dac06742
groob <vrancean+github@gmail.com>
2014-03-18 14:05:25 -0400

    changed quotes around provider"

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,8 +2,8 @@ class kibana::install {
   $kibana_vhost = $kibana::kibana_vhost
   ensure_packages(["git"])
   vcsrepo {'/opt/kibana':
-    ensure   => 'present',
-    provider => 'git',
+    ensure   => present,
+    provider => git,
     source   => 'git://github.com/elasticsearch/kibana.git',
     revision => 'v3.0.0',
   }
