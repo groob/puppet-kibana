56d0b9521659989830f97436146b225d1d2bc133
groob <vrancean+github@gmail.com>
2014-03-18 14:15:53 -0400

    Revert "removed provider line"
    This reverts commit 0a3d9c0bf0782f24a4df3d66960bcc7e0bb57f47.

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,7 +3,7 @@ class kibana::install {
   ensure_packages(["git"])
   vcsrepo {'/opt/kibana':
     ensure   => present,
-    # provider => git,
+    provider => git,
     source   => 'git://github.com/elasticsearch/kibana.git',
     revision => 'v3.0.0',
   }
